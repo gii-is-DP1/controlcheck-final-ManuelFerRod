c47d1a0abcef13c9ff03d01cf353ee885d5d49a7
practica <practica@10.1.12.182>
2022-01-21 10:55:19 +0100

    Primer Commit

--- src/main/java/org/springframework/samples/petclinic/care/CareProvisionRepository.java
+++ src/main/java/org/springframework/samples/petclinic/care/CareProvisionRepository.java
@@ -3,11 +3,23 @@ package org.springframework.samples.petclinic.care;
 import java.util.List;
 import java.util.Optional;
 
-public interface CareProvisionRepository{
-    List<CareProvision> findAll();        
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CareProvisionRepository extends CrudRepository<CareProvision, Integer>{
+    
+	@Query("SELECT CP FROM careProvision")
+	List<CareProvision> findAll();
+	
+	@Query("SELECT CP FROM careProvision WHERE CP.id=?1")
     Optional<CareProvision> findById(int id);
+	
     CareProvision save(CareProvision p);
-	//List<Care> findAllCares();
+    
+    @Query("SELECT C FROM cares")
+	List<Care> findAllCares();
+    
+    
     //List<Care> findCompatibleCares(String petTypeName);
     //Care findCareByName(String name);
     //List<CareProvision> findCaresProvidedByVisitId(Integer visitId);
